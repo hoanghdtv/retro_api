08201f9fa20217b8aa7248c259c27ba0e3af7736
hoanghdtv <hoanghdtv154@gmail.com>
2024-12-24 18:33:51 +0700

    ok

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,119 @@
-import mongoose from 'mongoose';
-import app from './app';
-import config from './config/config';
-import logger from './modules/logger/logger';
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 
-let server: any;
-mongoose.connect(config.mongoose.url).then(() => {
-  logger.info('Connected to MongoDB');
-  server = app.listen(config.port, () => {
-    logger.info(`Listening to port ${config.port}`);
-  });
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT || 3000;
+
+interface IUserIP {
+   ip: string;
+   isp: string;
+   country: string;
+   countryCode: string;
+   region: string;
+   city: string;
+   zip: string;
+   org: string;
+}
+
+// 2. Create a Schema corresponding to the document interface.
+const userSchema = new mongoose.Schema<IUserIP>({
+  ip: { type: String, required: true },
+  isp: { type: String, required: true },
+  countryCode: { type: String, required: true },
 });
 
-const exitHandler = () => {
-  if (server) {
-    server.close(() => {
-      logger.info('Server closed');
-      process.exit(1);
-    });
+// 3. Create a Model.
+const User = mongoose.model<IUserIP>('Google', userSchema);
+
+async function connectDB() {
+    await mongoose.connect(process.env.MONGODB_URL || "");
+    console.log("connect success to: " + process.env.MONGODB_URL);
+}
+
+
+async function getUserByIP(ip: string)
+{
+    return User.find({ip: ip});
+}
+
+function checkIpAddress(ip: string) {
+  const ipv4Pattern = 
+      /^(\d{1,3}\.){3}\d{1,3}$/;
+  const ipv6Pattern = 
+      /^([0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4}$/;
+  return ipv4Pattern.test(ip) || ipv6Pattern.test(ip);
+}
+
+connectDB().then((result) => {
+    
+}).catch(err => console.log(err));
+
+interface IPQuery {
+   ip:string
+}
+
+app.get("/", async (req: Request, res: Response) =>{
+  try {
+    const {ip} = req.query as unknown as IPQuery;
+    if(checkIpAddress(ip)){
+        let user = await handleIP(ip);
+        if(user != null){
+          res.send({success:"false", ip: user.ip, isp: user.isp, countryCode: user.countryCode});  
+        } else{
+          res.send({success:"true"}); 
+        }
+    }
+    else{
+      res.send({success:"true",error: "ip_invalid"});
+    }
+
+  } catch(err) {
+      console.log(err);
+      res.send({success:"true"});
+  }
+
+  
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at ${port}`);
+});
+
+async function handleIP(ip: string) {
+  let users : Array<any> = await getUserByIP(ip);
+  if(users.length == 0 ){
+      // check ip nay
+      const url = (process.env.IP_CHECK_URL || "http://ip-api.com/json/") + ip;
+      const response = await fetch(url);
+      const obj = await response.json();
+      
+      var user = new User({
+        ip:ip,
+        isp:obj.isp || "us",
+        countryCode: obj.countryCode || "us",
+        country: obj.country || "us",
+        zip: obj.zip || "100000",
+        region: obj.region || "us" 
+      });
+      
+      if(isBannedUser(user)){ 
+          console.log("banned ip :" + user.ip + " , country : " + user.countryCode + " , isp: " + user.isp);
+          await user.save();
+          return user;
+      } else {
+          return null;
+      }
   } else {
-    process.exit(1);
+    let user = users[0];
+    console.log("banned ip :" + user.ip + " , country : " + user.countryCode + " , isp: " + user.isp);
+    return users[0];
   }
-};
+}
 
-const unexpectedErrorHandler = (error: string) => {
-  logger.error(error);
-  exitHandler();
-};
-
-process.on('uncaughtException', unexpectedErrorHandler);
-process.on('unhandledRejection', unexpectedErrorHandler);
-
-process.on('SIGTERM', () => {
-  logger.info('SIGTERM received');
-  if (server) {
-    server.close();
-  }
-});
+function isBannedUser(user: IUserIP){
+  return user.countryCode.toLowerCase() == "in" || user.isp.toLowerCase().indexOf("google") != -1
+}
